bb0d85061a5912e3aec11f66e9305b88dffef6af
kathleenforgiarini <kath.for199@gmail.com>
2024-03-26 23:38:58 -0400

    creating index layour for properties

---
+++ RentRight/wwwroot/js/properties.js
@@ -0,0 +1,9 @@
+﻿function expandText(button) {
+    var cardBody = button.parentElement;
+    var cardText = cardBody.querySelector('.card-text');
+    var fullText = cardBody.querySelector('.card-text:last-of-type');
+
+    cardText.style.maxHeight = 'none'; // Remove a altura máxima
+    fullText.style.display = 'block'; // Exibe o texto completo
+    button.style.display = 'none'; // Esconde o botão "Leia mais"
+}
